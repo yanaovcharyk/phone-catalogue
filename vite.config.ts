c0c419b97637e60308a22ce248a455ae79673e42
Yana Ovcharyk <yanaovcharik@gmail.com>
2025-09-30 00:31:34 +0300

    fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nice-gadgets/',
+  base: '/phone-catalogue/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
